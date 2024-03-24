7662f3890669ed849b40e594433c49867504271f
Jade van Dorsten <jade@van-dorsten.net>
2024-03-24 18:56:21 -0500

    improve url display

--- assets/ts/og.ts
+++ assets/ts/og.ts
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const og: HTMLElement = og_container.querySelector('.og') as HTMLElement;
 
         const url = og.dataset.url || '';
+        const niceUrl = url.replace(/^(?:https?:\/\/)?(?:www\.)?/i, '');
         const title = og.dataset.title || '';
         const snippet = og.dataset.snippet || '';
         
@@ -22,7 +23,12 @@ document.addEventListener("DOMContentLoaded", () => {
             <div class="og-metadata">
                 <div>
                     ${title ? `<span class="og-title">${title}</span>` : ''}
-                    <span class="og-url">${url}</span>
+                    <div class="og-url-container">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="og-url-icon">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
+                        </svg>                  
+                        <span class="og-url">${niceUrl}</span>
+                    </div>
                 </div>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
