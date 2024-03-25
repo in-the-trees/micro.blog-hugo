df308355966b638b67c0364964ea400d27c38793
Jade van Dorsten <jade@van-dorsten.net>
2024-03-25 00:52:24 -0500

    add conditional rendering for if link is local

--- assets/ts/og.ts
+++ assets/ts/og.ts
@@ -8,6 +8,8 @@ document.addEventListener("DOMContentLoaded", () => {
         const niceUrl = url.replace(/^(?:https?:\/\/)?(?:www\.)?/i, '');
         const title = og.dataset.title || '';
         const snippet = og.dataset.snippet || '';
+
+        const isLocalUrl = window.location.hostname === new URL(url).hostname;
         
         let innerHTML: string = "";
 
@@ -30,16 +32,24 @@ document.addEventListener("DOMContentLoaded", () => {
                         <span class="og-url">${niceUrl}</span>
                     </div>
                 </div>
+                ${!isLocalUrl ? `
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
                 </svg>
+                ` : `
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                </svg>
+                `}
             </div>
         `;
 
         const a: HTMLAnchorElement = document.createElement('a');
         a.setAttribute('href', url);
-        a.setAttribute('target', '_blank');
-        a.setAttribute('rel', 'noopener noreferrer');
+        if (!isLocalUrl) {
+            a.setAttribute('target', '_blank');
+            a.setAttribute('rel', 'noopener noreferrer');
+        }
 
         og.innerHTML = innerHTML;
         og.removeAttribute('data-url');
